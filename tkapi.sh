e1f1b6e79f74d9735f389c3b823a5e5aa5ae41bd
Breyten Ernsting <bje@dds.nl>
2023-03-06 11:11:35 +0100

    Implement pagina and document downloading.

--- tkapi.sh
+++ tkapi.sh
@@ -2,10 +2,26 @@
 
 start_url=$1
 page_nr=1
+
 while [ "$start_url" != "null" ];
 do
+  fn="data/$page_nr.json"
   echo "$start_url"
-  curl -s "$start_url" >"$page_nr.json"
-  start_url=`jq -r '.["@odata.nextLink"]' "$page_nr.json"`
+  curl -s "$start_url" >"$fn"
+
+  main_documents=`jq -r 'select(.value[] |.DocumentNummer) |.value[] |.DocumentNummer' "$fn"`
+  sub_documents=`jq -r 'select(.value[] |.Document) |.value[] |.Document[] |.DocumentNummer' "$fn"`
+  for d in `echo -n $main_documents $sub_documents`; do
+    doc_url="https://www.tweedekamer.nl/downloads/document?id=$d"
+    curl -s "$doc_url" -o .tmp
+    doc_mime_type=`mimetype .tmp |awk '{print $2}'`
+    doc_ext=`grep "$doc_mime_type" /etc/mime.types |awk '{print $2}'`
+    cp .tmp "data/$d.$doc_ext"
+    echo $d
+    sleep 1
+    rm -f .tmp
+  done
+
+  start_url=`jq -r '.["@odata.nextLink"]' "$fn"`
   page_nr=`expr $page_nr + 1`
 done
