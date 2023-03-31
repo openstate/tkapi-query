23cebc1b2cfdefce919b4a628489f27755e1e26a
Breyten Ernsting <bje@dds.nl>
2023-03-31 17:31:41 +0200

    Add http status codes in files so it is possible to check what went wrong and what not.

--- tkapi.sh
+++ tkapi.sh
@@ -7,13 +7,13 @@ while [ "$start_url" != "null" ];
 do
   fn="data/$page_nr.json"
   echo "$start_url"
-  curl -s "$start_url" >"$fn"
+  curl -s "$start_url" -o "$fn" -w "%{http_code} %{url_effective}" >>pages.txt
 
   main_documents=`jq -r 'select(.value[] |.DocumentNummer) |.value[] |.DocumentNummer' "$fn"`
   sub_documents=`jq -r 'select(.value[] |.Document) |.value[] |.Document[] |.DocumentNummer' "$fn"`
   for d in `echo -n $main_documents $sub_documents`; do
     doc_url="https://www.tweedekamer.nl/downloads/document?id=$d"
-    curl -s "$doc_url" -o .tmp
+    curl -s "$doc_url" -o .tmp -w "%{http_code} %{url_effective}" >>documents.txt
     doc_mime_type=`mimetype .tmp |awk '{print $2}'`
     doc_ext=`grep "$doc_mime_type" /etc/mime.types |awk '{print $2}'`
     cp .tmp "data/$d.$doc_ext"
