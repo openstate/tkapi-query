7f96c412e142cbf0cd75a4134569c820d7a81e0c
Breyten Ernsting <bje@dds.nl>
2023-04-24 09:18:31 +0000

    Use document ids instead of document numbers.

--- tkapi.sh
+++ tkapi.sh
@@ -9,15 +9,18 @@ do
   echo "$start_url"
   curl -s "$start_url" -o "$fn" -w "%{http_code} %{url_effective}\n" >>pages.txt
 
-  main_documents=`jq -r 'select(.value[] |.DocumentNummer) |.value[] |.DocumentNummer' "$fn"`
-  sub_documents=`jq -r 'select(.value[] |.Document) |.value[] |.Document[] |.DocumentNummer' "$fn"`
+  main_documents=`jq -r 'select(.value[] |.ContentType) |.value[] |.Id' "$fn"`
+  sub_documents=`jq -r 'select(.value[] |.Document) |.value[] |.Document[] |.Id' "$fn"`
   for d in `echo -n $main_documents $sub_documents |tr ' ' '\n' | sort -nu`; do
     if [ "$d" != "null" ]; then
-      doc_url="https://www.tweedekamer.nl/downloads/document?id=$d"
+      doc_url="https://gegevensmagazijn.tweedekamer.nl/OData/v4/2.0/Document/$d/resource"
       echo $doc_url
       curl -s "$doc_url" -o .tmp -w "%{http_code} %{url_effective}\n" >>documents.txt
       doc_mime_type=`file .tmp |awk '{print $2}' |tr 'A-Z' 'a-z'`
       doc_ext=`grep "$doc_mime_type" /etc/mime.types |head -1 |awk '{print $2}'`
+      if [ "$doc_ext" == "" ]; then
+	doc_ext="pdf"
+      fi
       cp .tmp "data/$d.$doc_ext"
       echo $d
       sleep 1
